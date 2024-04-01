4e26f4462c5d590d07be2b1cadd4e4ed49a263da
Jônatas da Silva <jonatassilvasiqueira663@gmail.com>
2024-03-31 22:21:18 -0300

    quarto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ export default function app(){
       <section>
         <div className="bg-orange">
           <img src={iconSedans} alt="icone dos carros sedans" />
-          <h2 className="text-3xl">Sedans</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptatem expedita, recusandae quo doloribus facilis atque exercitationem et. Voluptatum ipsam sequi dolorum quibusdam, iure vel laboriosam provident voluptatibus explicabo sapiente?</p>
-          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+          <h2 className="font-shouders">Sedans</h2>
+          <p className="font-lexend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptatem expedita, recusandae quo doloribus facilis atque exercitationem et. Voluptatum ipsam sequi dolorum quibusdam, iure vel laboriosam provident voluptatibus explicabo sapiente?</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
         <div className="bg-green">
           <img src={iconSuvs} alt="icone dos carros suvs" />
-          <h2>Suvs</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, numquam blanditiis? Numquam amet, illo, minus at, quaerat natus ex magni nesciunt aperiam debitis accusamus sapiente omnis provident? Corrupti, a fugiat.</p>
-          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais </a>
+          <h2 className="font-shouders">Suvs</h2>
+          <p className="font-lexend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, numquam blanditiis? Numquam amet, illo, minus at, quaerat natus ex magni nesciunt aperiam debitis accusamus sapiente omnis provident? Corrupti, a fugiat.</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais </a>
         </div>
         <div className="bg-dark-green">
           <img src={iconLuxury} alt="icone dos carros luxury" />
-          <h2>Luxury</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam fugit commodi, deleniti ducimus reprehenderit odit nulla modi temporibus a architecto exercitationem at. Saepe voluptatum eum eaque numquam molestias illo ipsum?</p>
-          <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+          <h2 className="font-shouders" >Luxury</h2>
+          <p className="font-lexend">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam fugit commodi, deleniti ducimus reprehenderit odit nulla modi temporibus a architecto exercitationem at. Saepe voluptatum eum eaque numquam molestias illo ipsum?</p>
+          <a className="font-lexend" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
 
         </div>
       </section>
